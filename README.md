b3823f805209ff64bf877fb8d1b5cc7598401a10
Dave Bascom <dave@cumulocreative.com>
2016-10-10 08:29:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic Gulp
+A basic webpage tooling setup, using gulp
+
+
+The completed files are found inside the `build` folder.
---
+++ gulpfile.babel.js
@@ -0,0 +1,58 @@
+/* jshint esversion:6 */
+
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+
+import less from 'gulp-less';
+import autoprefixer from 'gulp-autoprefixer';
+import cssnano from 'gulp-cssnano';
+import base64 from 'gulp-base64';
+
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import gulpIf from 'gulp-if';
+
+// Preprocess less
+gulp.task( 'less', function() {
+  return gulp.src( 'app/less/main.less' ) // specific file since I'm importing all less into main.less
+    .pipe( less() )
+    .pipe( base64() ) // options available: https://www.npmjs.com/package/gulp-base64
+    .pipe( autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe( gulpIf( '*.css', cssnano()))
+    .pipe( gulp.dest( 'build' ) )
+    .pipe( browserSync.reload({
+      stream: true
+    }));
+});
+
+// Concat javascript.
+// This only works if all JS is in the same directory. Use gulp-useref if multiple dirs are needed
+// Files are specified by name to determine order.
+gulp.task( 'scripts', function() {
+  return gulp.src( ['app/js/main.js', 'app/js/home.js'] )
+    .pipe( concat( 'scripts.js' ))
+    .pipe( gulpIf( '*.js', uglify()))
+    .pipe( gulp.dest( 'build' ))
+    .pipe( browserSync.reload({
+      stream: true
+    }));
+});
+
+// Hot reload
+gulp.task( 'browserSync', function() {
+  browserSync.init( {
+    server: {
+      baseDir: 'build'
+    }
+  });
+});
+
+// Watcher task
+gulp.task( 'watch', [ 'browserSync', 'less' ], function() {
+  gulp.watch( 'app/less/*.less', [ 'less' ]);
+  gulp.watch( 'app/js/*.js', [ 'scripts' ]); // watches all .js files, even if not concatenated
+  gulp.watch( 'build/*.html', browserSync.reload );
+});
